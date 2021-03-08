aa99387e3693e2c32af1b9ec7bc57a5b1402f428
xCmac <dsemac@gmail.com>
2021-03-07 21:11:17 -0600

    first commit

---
+++ PDF2IMAGE/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace PDF2IMAGE
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_selectPDF = new System.Windows.Forms.Button();
+            this.button_output = new System.Windows.Forms.Button();
+            this.button_generate = new System.Windows.Forms.Button();
+            this.textbox_selectedPDF = new System.Windows.Forms.TextBox();
+            this.textbox_output = new System.Windows.Forms.TextBox();
+            this.label_PageCount = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button_selectPDF
+            // 
+            this.button_selectPDF.Location = new System.Drawing.Point(12, 12);
+            this.button_selectPDF.Name = "button_selectPDF";
+            this.button_selectPDF.Size = new System.Drawing.Size(89, 23);
+            this.button_selectPDF.TabIndex = 0;
+            this.button_selectPDF.Text = "Select PDF";
+            this.button_selectPDF.UseVisualStyleBackColor = true;
+            this.button_selectPDF.Click += new System.EventHandler(this.button_selectPDF_Click);
+            // 
+            // button_output
+            // 
+            this.button_output.Location = new System.Drawing.Point(12, 41);
+            this.button_output.Name = "button_output";
+            this.button_output.Size = new System.Drawing.Size(89, 23);
+            this.button_output.TabIndex = 1;
+            this.button_output.Text = "Output Folder";
+            this.button_output.UseVisualStyleBackColor = true;
+            this.button_output.Click += new System.EventHandler(this.button_output_Click);
+            // 
+            // button_generate
+            // 
+            this.button_generate.Enabled = false;
+            this.button_generate.Location = new System.Drawing.Point(12, 112);
+            this.button_generate.Name = "button_generate";
+            this.button_generate.Size = new System.Drawing.Size(356, 23);
+            this.button_generate.TabIndex = 2;
+            this.button_generate.Text = "Generate Images";
+            this.button_generate.UseVisualStyleBackColor = true;
+            this.button_generate.Click += new System.EventHandler(this.button_generate_Click);
+            // 
+            // textbox_selectedPDF
+            // 
+            this.textbox_selectedPDF.Location = new System.Drawing.Point(107, 12);
+            this.textbox_selectedPDF.Name = "textbox_selectedPDF";
+            this.textbox_selectedPDF.ReadOnly = true;
+            this.textbox_selectedPDF.Size = new System.Drawing.Size(261, 23);
+            this.textbox_selectedPDF.TabIndex = 3;
+            this.textbox_selectedPDF.TextChanged += new System.EventHandler(this.textboxSelectedPDF_TextChanged);
+            // 
+            // textbox_output
+            // 
+            this.textbox_output.Location = new System.Drawing.Point(107, 41);
+            this.textbox_output.Name = "textbox_output";
+            this.textbox_output.ReadOnly = true;
+            this.textbox_output.Size = new System.Drawing.Size(261, 23);
+            this.textbox_output.TabIndex = 4;
+            this.textbox_output.TextChanged += new System.EventHandler(this.textBoxOutput_TextChanged);
+            // 
+            // label_PageCount
+            // 
+            this.label_PageCount.AutoSize = true;
+            this.label_PageCount.Location = new System.Drawing.Point(12, 94);
+            this.label_PageCount.Name = "label_PageCount";
+            this.label_PageCount.Size = new System.Drawing.Size(96, 15);
+            this.label_PageCount.TabIndex = 5;
+            this.label_PageCount.Text = "label_PageCount";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 141);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(356, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 171);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label_PageCount);
+            this.Controls.Add(this.textbox_output);
+            this.Controls.Add(this.textbox_selectedPDF);
+            this.Controls.Add(this.button_generate);
+            this.Controls.Add(this.button_output);
+            this.Controls.Add(this.button_selectPDF);
+            this.Name = "Form1";
+            this.Text = "PDF2Image";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_selectPDF;
+        private System.Windows.Forms.Button button_output;
+        private System.Windows.Forms.Button button_generate;
+        private System.Windows.Forms.TextBox textbox_selectedPDF;
+        private System.Windows.Forms.TextBox textbox_output;
+        private System.Windows.Forms.Label label_PageCount;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ PDF2IMAGE/Form1.cs
@@ -0,0 +1,145 @@
+﻿using Docnet.Core;
+using Docnet.Core.Converters;
+using Docnet.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDF2IMAGE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_selectPDF_Click(object sender, EventArgs e)
+        {
+            textbox_selectedPDF.Text = getFilePath();
+        }
+
+        private void button_output_Click(object sender, EventArgs e)
+        {
+            textbox_output.Text = getFolderPath();
+        }
+
+        private async void button_generate_Click(object sender, EventArgs e)
+        {
+            button_selectPDF.Enabled = false;
+            button_output.Enabled = false;
+            button_generate.Enabled = false;
+
+            var progress = new Progress<int>(value =>
+            {
+                progressBar1.Value = value;
+            });
+
+            await Task.Run(() => generate(progress));
+
+            button_selectPDF.Enabled = true;
+            button_output.Enabled = true;
+            textbox_selectedPDF.Text = string.Empty;
+            textbox_output.Text = string.Empty;
+            progressBar1.Value = 0;
+        }
+
+        private void generate(IProgress<int> progress)
+        {
+            using var docReader = DocLib.Instance.GetDocReader(textbox_selectedPDF.Text, new PageDimensions(1080, 1920));
+
+            var pageCount = docReader.GetPageCount();
+
+            for (int i = 0; i < pageCount; i++)
+            {
+                using var pageReader = docReader.GetPageReader(i);
+
+                var rawBytes = pageReader.GetImage();
+
+                var width = pageReader.GetPageWidth();
+                var height = pageReader.GetPageHeight();
+
+                var characters = pageReader.GetCharacters();
+
+                using var bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+
+                AddBytes(bmp, rawBytes);
+                using var stream = new MemoryStream();
+
+                bmp.Save(stream, ImageFormat.Png);
+
+                File.WriteAllBytes($"{textbox_output.Text}/{i}.png", stream.ToArray());
+                var percentComplete = (i * 100) / pageCount;
+                progress.Report(percentComplete);
+            }
+        }
+
+        private static void AddBytes(Bitmap bmp, byte[] rawBytes)
+        {
+            var rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+
+            var bmpData = bmp.LockBits(rect, ImageLockMode.WriteOnly, bmp.PixelFormat);
+            var pNative = bmpData.Scan0;
+
+            Marshal.Copy(rawBytes, 0, pNative, rawBytes.Length);
+            bmp.UnlockBits(bmpData);
+        }
+        private string getFilePath()
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "Select PDF";
+            fdlg.InitialDirectory = @"c:\";
+            fdlg.Filter = "Pdf Files|*.pdf";
+            fdlg.FilterIndex = 2;
+            fdlg.RestoreDirectory = true;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                return fdlg.FileName;
+            }
+            return "";
+        }
+
+        private string getFolderPath()
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                return folderDlg.SelectedPath;
+            }
+
+            return "";
+        }
+
+        private void textboxSelectedPDF_TextChanged(object sender, EventArgs e)
+        {
+            setButtonVisibility();
+        }
+
+        private void textBoxOutput_TextChanged(object sender, EventArgs e)
+        {
+            setButtonVisibility();
+        }
+
+        private void setButtonVisibility()
+        {
+            if ((textbox_selectedPDF.Text != string.Empty) && (textbox_output.Text != string.Empty))
+            {
+                button_generate.Enabled = true;
+            }
+            else
+            {
+                button_generate.Enabled = false;
+            }
+        }
+    }
+}
